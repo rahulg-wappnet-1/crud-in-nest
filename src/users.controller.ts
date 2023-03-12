79c8bdc10c6d1b739fd72659544c8322251dd973
Rahul Gupta <rahulg.wappnet@gmail.com>
2023-03-13 00:56:29 +0530

    user crud without db

---
+++ src/users.controller.ts
@@ -0,0 +1,48 @@
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import {CreateUserDto} from './dto/createuserdto'
+
+let USERS = []
+
+
+
+@Controller('/users')
+export class UserController{
+    @Post()
+    addUser(@Body() createUserDto: CreateUserDto){
+        USERS.push(createUserDto)
+        return {
+            message:'user added'
+        }
+    }
+
+    @Get()
+    getUsers(){
+        return USERS
+    }
+
+    @Get(":id")
+    getUser(@Param("id") id:number){
+        return USERS.find(user => user.id === +id)
+    }
+
+    @Put(":id")
+    updateUser(@Param("id") id:number, @Body() updateUserDTO : CreateUserDto){
+        const userIdx  =  USERS.findIndex(user => +user.id === +id)
+        if(!userIdx){
+            return;
+        }
+        USERS[userIdx] = updateUserDTO
+        return{
+            messsage:'user updated'
+        }
+    }
+
+
+    @Delete(":id")
+    daleteUser(@Param("id") id:number){
+        USERS = USERS.filter(user => +user.id !== +id)
+        
+    }
+
+
+}
