7279db31a67e6b14de02f61c4c242454c4228d90
Rahul Gupta <rahulg.wappnet@gmail.com>
2023-03-14 00:33:19 +0530

    different ways of adding dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './users.controller';
+import{UsersStore} from './store/users.stores'
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [],
+  providers: [UsersStore] // ---2
+  //providers:[{provide:'STORE' , useClass:UsersStore}]  --- 1
+  //providers:[{provide:UsersStore , useClass:UsersStore}], --- 2
+
+  //value providers
 })
 export class AppModule {}
--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Ip, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Inject, Ip, Param, Post, Put } from "@nestjs/common";
 import {CreateUserDto} from './dto/createuserdto'
+import { UsersStore } from "./store/users.stores";
 
 let USERS = []
 
@@ -7,43 +8,53 @@ let USERS = []
 
 @Controller('/users')
 export class UserController{
-    @Post()
-    addUser(@Body() createUserDto: CreateUserDto){
-        USERS.push(createUserDto)
-        return {
-            message:'user added'
-        }
-    }
+    // @Post()
+    // addUser(@Body() createUserDto: CreateUserDto){
+    //     USERS.push(createUserDto)
+    //     return {
+    //         message:'user added'
+    //     }
+    // }
 
-    @Get()
-    getUsers(@Ip() ip:string){
-        console.log(ip) // gives the ip of request
-        return USERS
-    }
+    // @Get()
+    // getUsers(@Ip() ip:string){
+    //     console.log(ip) // gives the ip of request
+    //     return USERS
+    // }
 
-    @Get(":id")
-    getUser(@Param("id") id:number){
-        return USERS.find(user => user.id === +id)
-    }
+    // @Get(":id")
+    // getUser(@Param("id") id:number){
+    //     return USERS.find(user => user.id === +id)
+    // }
 
-    @Put(":id")
-    updateUser(@Param("id") id:number, @Body() updateUserDTO : CreateUserDto){
-        const userIdx  =  USERS.findIndex(user => +user.id === +id)
-        if(!userIdx){
-            return;
-        }
-        USERS[userIdx] = updateUserDTO
-        return{
-            messsage:'user updated'
-        }
-    }
+    // @Put(":id")
+    // updateUser(@Param("id") id:number, @Body() updateUserDTO : CreateUserDto){
+    //     const userIdx  =  USERS.findIndex(user => +user.id === +id)
+    //     if(!userIdx){
+    //         return;
+    //     }
+    //     USERS[userIdx] = updateUserDTO
+    //     return{
+    //         messsage:'user updated'
+    //     }
+    // }
 
 
-    @Delete(":id")
-    daleteUser(@Param("id") id:number){
-        USERS = USERS.filter(user => +user.id !== +id)
+    // @Delete(":id")
+    // daleteUser(@Param("id") id:number){
+    //     USERS = USERS.filter(user => +user.id !== +id)
 
-    }
+    // }
+
+//************************************************************************************** *
+
+    constructor(private store: UsersStore){
+        console.log(this.store)
+    } // --- 2
+
+    // constructor(@Inject('STORE') private store: UsersStore ){
+    //     console.log(this.store)
+    // } --- 1
 
 
 }
